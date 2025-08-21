005862b4dc5619a162bdb73f0462ebd7615534b9
MaliKCoderGreen <green.malik5@gmail.com>
2025-08-21 12:11:26 -0400

    Changed docker network config; Prometheus yml for Prometheus

---
+++ user-activity-tracker/src/main/java/com/malik/streams/user_activity_tracker/repository/StatsRepository.java
@@ -0,0 +1,4 @@
+package com.malik.streams.user_activity_tracker.repository;
+
+public class StatsRepository {
+}
---
+++ user-activity-tracker/src/main/java/com/malik/streams/user_activity_tracker/service/StatsService.java
@@ -0,0 +1,4 @@
+package com.malik.streams.user_activity_tracker.service;
+
+public class StatsService {
+}
